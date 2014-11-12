71880dc0e7a04b4c4ce32e66be99757b977f6506
lancedjack <paton89@gmail.com>
2014-11-12 22:44:27 +0000

    Moved some code around

--- random2.py
+++ random2.py
@@ -1,3 +1,5 @@
+#Determine user input
+
 x = (raw_input("What do you choose? Rock? Paper? Or the mighty Scissors?"))
 if x == ("Rock" or "rock"):
     print ("You selected Rock")
@@ -7,27 +9,32 @@ else:
     x == ("Paper" or "paper")
     print ("You selected Paper")
 
+#Randomise computer input
+
 import random
 cpu = ["Scissors", "Rock", "Paper"]
 print ("Computer selects") 
-print (random.choice(cpu))
+cpu2 = (raw_input("Select for testing")) #(random.choice(cpu)) For random selection generation
+print ("Computer selects", cpu2)
 
-if x == (("Rock" or "rock") and cpu == ("Rock" or "rock")):
+#sort results
+
+if x == (("Rock" or "rock") and cpu2 == ("Rock" or "rock")):
     print ("Draw! Go again!")
-elif x == (("Rock" or "rock") and cpu == ("Paper" or "paper")):
+elif x == (("Rock" or "rock") and cpu2 == ("Paper" or "paper")):
     print ("Down your bevvy")
-elif x == (("Rock" or "rock") and cpu == ("Scissors" or "scissors")):
+elif x == (("Rock" or "rock") and cpu2 == ("Scissors" or "scissors")):
     print ("One minute")
-elif x == (("Paper" or "paper") and cpu == ("Paper" or "paper")):
+elif x == (("Paper" or "paper") and cpu2 == ("Paper" or "paper")):
     print ("Draw! Go again!")
-elif x == (("Paper" or "paper") and cpu == ("Scissors" or "scissors")):
+elif x == (("Paper" or "paper") and cpu2 == ("Scissors" or "scissors")):
     print ("Down your bevvy")
-elif x == (("Paper" or "paper") and cpu == ("Rock" or "rock")):
+elif x == (("Paper" or "paper") and cpu2 == ("Rock" or "rock")):
     print ("One minute")
-elif x == (("Scissors" or "scissors") and cpu == ("Scissors" or "scissors")):
+elif x == (("Scissors" or "scissors") and cpu2 == ("Scissors" or "scissors")):
     print ("Draw! Go again!")
-elif x == (("Scissors" or "scissors") and cpu == ("Rock" or "rock")):
+elif x == (("Scissors" or "scissors") and cpu2 == ("Rock" or "rock")):
     print ("Down your bevvy")
 else: 
-	x == (("Scissors" or "scissors") and cpu == ("Paper" or "paper")):
+    x == (("Scissors" or "scissors") and cpu2 == ("Paper" or "paper"))
     print ("One minute")
